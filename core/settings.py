1218c908640644c267109d5faf0ed553353f1407
Wesley-dmg <123087152+Wesley-dmg@users.noreply.github.com>
2025-09-25 12:42:15 +0100

    suppression de middleware et  mise a jour  du  fichier requierement

--- core/settings.py
+++ core/settings.py
@@ -70,7 +70,7 @@ MIDDLEWARE = [
     "django.contrib.auth.middleware.AuthenticationMiddleware",
     "django.contrib.messages.middleware.MessageMiddleware",
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
-    "apps.users.middleware.RedirectIfAuthenticatedMiddleware",
+    # "apps.users.middleware.RedirectIfAuthenticatedMiddleware",
 ]
 
 ROOT_URLCONF = "core.urls"
